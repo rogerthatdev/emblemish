9dbf965feab9af6954cdb549276e389431b82df3
Roger Martinez <31829545+rogerthatdev@users.noreply.github.com>
2023-06-22 16:25:48 -0700

    Update app.py

--- website/app.py
+++ website/app.py
@@ -12,8 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os 
-
+import os
 
 from flask import Flask, current_app
 from views.campaigns import campaigns_bp
